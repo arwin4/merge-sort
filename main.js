99edfcf6fedbe8582f5892ad1e00dcfa3de5aab4
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-05-31 16:11:18 +0200

    Add comments, remove unused code, handle bad input

--- main.js
+++ main.js
@@ -6,50 +6,56 @@ const array5 = [438, 181, 455, 109, 620, 805]; // large, nonconsecutive numbers
 const array6 = []; // nothing to sort, i.e. sorted
 const array7 = [2]; // one item, i.e. sorted
 const array8 = [4, -2, 3, -243]; // negative numbers
-const array9 = [4, 2, 2, 6, 5, 3, 8, 1, 7, 2, 3, 12, 11, 9, 10];
+const array9 = [4, 2, 2, 6, 5, 3, 8, 1, 7, 2, 3, 12, 11, 9, 10]; // duplicates
 const array10 = [5, 2, 1, 3, 6, 4]; // Example from CS50: https://youtu.be/Ns7tGNbtvV4
 const array11 = [5, 2, 1, 3];
 const array12 = [9, 3, 7, 5, 6, 4, 8, 2, 10]; // https://www.youtube.com/watch?v=mB5HXBb_HY8
 const array13 = [5, 2, 1, 3, 4];
 
 function mergeSort(arr) {
+  // Handle incorrect input
+  if (!Array.isArray(arr) || arr.length < 1)
+    return 'Please provide a valid sortable array. Format: [3, 1, -2]';
   // Base case: just one item, so it's already sorted
-  if (arr.length === 1) {
-    return arr;
-  } else {
-    // Split into left and right arrays
-    // Math.floor() accommodates any parity of array length
-    const lengthLeft = Math.floor(arr.length / 2);
-    const right = arr.splice(lengthLeft);
-    const left = arr;
+  if (arr.length === 1) return arr;
 
-    const newLeft = mergeSort(left);
-    const newRight = mergeSort(right);
-    if (newRight === undefined) {
-      return [newLeft];
-    } else return merge(newLeft, newRight);
-  }
+  // Split into left and right arrays
+  // Math.floor() accommodates any parity of array length
+  const lengthLeft = Math.floor(arr.length / 2);
+  const right = arr.splice(lengthLeft);
+  const left = arr;
+
+  const newLeft = mergeSort(left);
+  const newRight = mergeSort(right);
+  return merge(newLeft, newRight);
 }
 
 function merge(left, right) {
-  console.log(left, right);
   let merged = [];
-  let leftPointer = left[0] || left; // TODO: remove ||?
-  let rightPointer = right[0] || right;
+  let leftPointer = left[0];
+  let rightPointer = right[0];
+
+  // As long as there are values to compare, keep comparing and adding to the
+  // merged array.
+  // Remove values added to the array (the smallest values) from the original
+  // arrays. This way, we can exit the loop when no comparing is left to do.
   while (left.length > 0 && right.length > 0) {
     if (leftPointer < rightPointer) {
       merged.push(leftPointer);
+      // Remove the pushed value from the original array
       left = left.slice(1);
-      console.log(merged);
+      // Ready the pointer to compare the next value.
+      // It was moved to index 0 at the above step.
       leftPointer = left[0];
     } else {
       merged.push(rightPointer);
       right = right.slice(1);
-      console.log(merged);
       rightPointer = right[0];
     }
   }
 
+  // There's just one value left, so we know that it's the largest. So figure
+  // out on which side it is and at it to the end of the merged array.
   while (left.length > 0) {
     merged.push(left[0]);
     left = left.slice(1);
@@ -62,10 +68,17 @@ function merge(left, right) {
   return merged;
 }
 
-// mergeSort(array2);
+console.log(mergeSort(array1));
+console.log(mergeSort(array1));
+console.log(mergeSort(array3));
+console.log(mergeSort(array4));
+console.log(mergeSort(array5));
+console.log(mergeSort(array6));
+console.log(mergeSort(array7));
+console.log(mergeSort(array8));
 console.log(mergeSort(array9));
-// console.log(mergeSort(array9));
-// mergeSort(array8);
-// console.log(mergeSort(array10));
-// console.log(mergeSort(array11));
-// console.log(mergeSort(array12));
+console.log(mergeSort(array10));
+console.log(mergeSort(array11));
+console.log(mergeSort(array12));
+console.log(mergeSort(array13));
+console.log(mergeSort());
